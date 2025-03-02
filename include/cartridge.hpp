aabefd6c04cca1c8d509f0b89502ccfd5cf8aad2
Jesse Purcell <jesse.purcell@my.edu.au>
2025-03-03 00:30:56 +1000

    feat: add getter for ROM data in Cartridge class

--- include/cartridge.hpp
+++ include/cartridge.hpp
@@ -13,6 +13,8 @@ class Cartridge {
   std::optional<uint8_t> loadROM(const std::string &filename);
   std::optional<uint8_t> readByte(uint16_t address) const;
 
+  std::vector<uint8_t> getROM() const { return rom; }
+
  private:
   std::vector<uint8_t> rom;
 };
