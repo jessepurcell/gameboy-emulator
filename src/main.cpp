8b61845154dee0e92d9d041952f72be374be199a
Jesse Purcell <jesse.purcell@my.edu.au>
2025-03-03 00:31:02 +1000

    feat: implement main function to load ROM and print hex dump

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
-int main() { return 0; }
+#include <iostream>
+
+#include "../include/cartridge.hpp"
+#include "../include/utils.hpp"
+
+int main() {
+  Cartridge cart;
+  auto result = cart.loadROM("tetris.gb");
+  if (result) {
+    return 1;
+  }
+
+  printHexDump(cart.getROM());
+
+  return 0;
+}
